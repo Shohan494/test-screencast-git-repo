af6700faa3f52da6936af2b819f3b3a97cfd09e3
Shohan494 <shohan12224943@gmail.com>
2020-05-14 12:34:56 +0600

    local master and develop pushed to origin master and develop notes

--- index.php
+++ index.php
@@ -14,4 +14,21 @@ echo "<br>";
 echo "we are in local develop";
 
 echo "<br>";
+
+echo "made some changes in this file, index.php";
+
+echo "<br>";
+
+echo "git add .";
+
+echo "<br>";
+
+echo "git commit -m 'made some changes in this file, index.php'";
+
+echo "<br>";
+
+// BEFORE PUSHING DEVELOP FIRST, BETTER WE PUSH OUR MASTER BRANCH
+
+echo "git push origin develop"
+
 ?>
